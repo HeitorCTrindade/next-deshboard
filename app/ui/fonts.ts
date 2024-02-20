384f99bcedf69437996b77e8b6899ae5e0cb804b
HeitorCTrindade <heitor-trindade@hotmail.com>
2024-02-20 20:53:39 -0300

    feat: initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { DM_Sans } from 'next/font/google'
+import { Inter } from 'next/font/google'
+
+export const dmSans = DM_Sans({ weight: ['300'], subsets: ['latin-ext'] });
+export const secundaryFont = Inter({ weight: ['900'], subsets: ['latin'] });
