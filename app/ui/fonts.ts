a9a8c27aa2c852745d9834264619d10e793f1d57
HeitorCTrindade <heitor-trindade@hotmail.com>
2024-02-21 23:26:43 -0300

    fix: font-type

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { DM_Sans } from 'next/font/google'
-import { Inter } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
 
 export const dmSans = DM_Sans({ weight: ['300'], subsets: ['latin-ext'] });
-export const secundaryFont = Inter({ weight: ['900'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['700'], subsets: ['latin'] });
